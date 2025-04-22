393d062fd95fc81272e1fb58b77bcb77a09a627e
Roman Ponomarev <xgerfy@gmail.com>
2025-04-22 16:46:31 +0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	reactStrictMode: true,
+	poweredByHeader: false,
+	async rewrites() {
+		return [
+			{
+				source: '/uploads/:path*',
+				destination: `${process.env.SERVER_URL}/uploads/:path*`
+			}
+		]
+	}
+	/* config options here */
+}
 
-export default nextConfig;
+export default nextConfig
