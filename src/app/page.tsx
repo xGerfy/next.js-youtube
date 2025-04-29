44475cbd33d8eadf984dc16e32c6a7d2eb081375
Roman Ponomarev <xgerfy@gmail.com>
2025-04-29 16:29:03 +0400

    add: metadata and Heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 import { videoService } from '@/services/video.service'
+import { Heading } from '@/ui/Heading'
 import { VideoItem } from '@/ui/video-item/VideoItem'
 import { Flame } from 'lucide-react'
+import type { Metadata } from 'next'
 
 import { Explore } from './explore/Explore'
 
 export const revalidate = 100
+
+export const metadata: Metadata = {
+	title: 'YouTube',
+	description: 'Best video platform',
+	alternates: {
+		canonical: '/'
+	},
+	openGraph: {
+		type: 'website',
+		url: '/',
+		title: 'YouTube'
+	}
+}
 
 export default async function Home() {
 	const data = await videoService.getTrendingVideos()
 
 	return (
 		<section>
-			<section>
-				<h2>Trending</h2>
+			<section className='mb-10'>
+				<Heading Icon={Flame}>Trending</Heading>
 				<div className='grid grid-cols-6 gap-6'>
 					{data.data &&
 						data.data.map(video => (
